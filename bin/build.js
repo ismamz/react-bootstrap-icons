9ba8612e1521e174fed147b031306132a6b2a8a5
Ismael Martínez <hola@isma.uy>
2020-03-22 16:48:02 -0300

    v1.0.1-alpha3

--- bin/build.js
+++ bin/build.js
@@ -32,7 +32,7 @@ fs.readdirSync(iconsDir).forEach((file) => {
       return (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
+          viewBox="0 0 16 16"
           width={size}
           height={size}
           fill={color}
--- src/icons/brightness-alt-low-fill.js
+++ src/icons/brightness-alt-low-fill.js
@@ -7,7 +7,7 @@ const BrightnessAltLowFill = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
+      viewBox="0 0 16 16"
       width={size}
       height={size}
       fill={color}
